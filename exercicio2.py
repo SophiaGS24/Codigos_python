bbe3ddd61f04fc176cd9583b15335d71dcb60506
Sophia Giordano Seabra <sophiagiords@gmail.com>
2025-09-23 21:15:52 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,7 +1,9 @@
 #EXERCICIO 1.2
+
 evento = "Taylor Swift, Eras Tour"
 Ingresso = 500 
 quantidade = 80
+
 
 print(f"Escolha o evento: {evento}")
 print(f"O valor unitário do ingresso é {ingresso}")
@@ -19,22 +21,6 @@ else:
     print(f"Número de ingressos indisponível no momento")
     
 
-#EXERCICIO2
-lista_de_numeros = [9, 20, 40]
-print(lista_de_numeros)
-numero_digitado = int(input("Digite o numero: "))
-lista_de_numeros.append(numero_digitado)
-print(f"Lista após inserir o numero 20 --> {lista_de_numeros}")
-
-a = ["Joao","Maria"]
-a.insert(0, input("Qual o nome: "))
-print(a)
-
-#EXERCICIO3
-paciente = ["Joao","Maria"]
-paciente.insert(0, input("Qual o nome do paciente: "))
-print(paciente)
-
-paciente(input("paciente atendido: "))
 
    
+
